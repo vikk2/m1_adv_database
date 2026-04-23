3acec94e65450b75668abf4a1d2269c4b25e0e6e
weesal <weesal@weesals-MacBook-Pro.local>
2026-04-23 19:30:08 +0700

    first-commit

--- school_project/core/db.py
+++ school_project/core/db.py
@@ -1,6 +1,9 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["school_db"]
 
 students_col = db["students"]
